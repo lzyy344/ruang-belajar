4cb378c3441b6f6d4e86d3ef6bec53bbd5d7f7a5
lzyy344 <andikagans344@gmail.com>
2026-09-04 13:38:03 +0700

    fix: simplify web eslintrc - remove typescript-eslint plugin

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,35 +1,10 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2022: true,
-    node: true,
-  },
-  extends: [
-    'next/core-web-vitals',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'prettier',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    ecmaFeatures: { jsx: true },
-    project: './tsconfig.json',
-  },
-  plugins: ['@typescript-eslint', 'react-refresh', 'prettier'],
+﻿module.exports = {
+  extends: ['next/core-web-vitals'],
   rules: {
-    'react-refresh/only-export-components': 'off',
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-explicit-any': 'warn',
-    'prettier/prettier': 'error',
+    'no-console': 'off',
+    'no-unused-vars': 'off',
     'react/no-unescaped-entities': 'off',
+    '@next/next/no-html-link-for-pages': 'off',
   },
-  settings: {
-    react: { version: '18' },
-    'import/resolver': {
-      typescript: { project: './tsconfig.json' },
-      node: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
-    },
-  },
+  ignorePatterns: ['dist/', 'node_modules/', '.next/'],
 };
