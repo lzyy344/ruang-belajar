832453f3aaf4fdf272edded8c203ff9c9461129f
lzyy344 <andikagans344@gmail.com>
2026-09-04 12:59:11 +0700

    fix: add typescript-eslint parser to all packages

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -1,7 +1,8 @@
 ﻿module.exports = {
   env: { es2022: true, node: true },
   extends: ['eslint:recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 2022, sourceType: 'module' },
-  rules: { 'no-console': 'off', 'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }] },
+  rules: { 'no-console': 'off', 'no-unused-vars': 'off' },
   ignorePatterns: ['dist/', 'node_modules/'],
 };
