646a323fa5b659eb373d05e06d48ba3176086c9b
lzyy344 <andikagans344@gmail.com>
2026-09-04 13:24:28 +0700

    fix: add browser env and disable require-yield in ai-providers eslint

--- packages/ai-providers/.eslintrc.js
+++ packages/ai-providers/.eslintrc.js
@@ -1,8 +1,12 @@
 ﻿module.exports = {
-  env: { es2022: true, node: true },
+  env: { es2022: true, node: true, browser: true },
   extends: ['eslint:recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 2022, sourceType: 'module' },
-  rules: { 'no-console': 'off', 'no-unused-vars': 'off' },
+  rules: {
+    'no-console': 'off',
+    'no-unused-vars': 'off',
+    'require-yield': 'off',
+  },
   ignorePatterns: ['dist/', 'node_modules/'],
 };
