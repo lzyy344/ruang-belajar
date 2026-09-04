a332aa51851b52cb15ab38d693002e6af2c8e82c
lzyy344 <andikagans344@gmail.com>
2026-09-04 12:54:14 +0700

    fix: simplify eslint config - remove typescript-eslint plugin dependency

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -1,23 +1,7 @@
 ﻿module.exports = {
-  env: {
-    es2022: true,
-    node: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2022,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint'],
-  rules: {
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-require-imports': 'off',
-    'no-console': 'off',
-  },
+  env: { es2022: true, node: true },
+  extends: ['eslint:recommended'],
+  parserOptions: { ecmaVersion: 2022, sourceType: 'module' },
+  rules: { 'no-console': 'off', 'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }] },
   ignorePatterns: ['dist/', 'node_modules/'],
 };
